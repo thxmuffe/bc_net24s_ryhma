e223feada79c18e82b77813ea75598f6350c8662
Paavo Penttila <thxmuffe@hotmail.com>
2025-01-19 22:35:13 +0200

    Add model solution to first exercise

--- src/part1_kertausta/Kertaus1/Program.cs
+++ src/part1_kertausta/Kertaus1/Program.cs
@@ -1,12 +1,42 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!" + args[0]);
+        Console.WriteLine("start " + String.Join(", ", args));
+
+        // Try to parse first argument as number
+        if (int.TryParse(args.FirstOrDefault(), out int num))
+        {
+            num++;
+
+            if (num > 10) {
+                Console.WriteLine("Reached 10!");
+                Environment.Exit(0);
+            }
+
+            using (var p = new Process()) {
+                // Get current program executable
+                p.StartInfo.FileName =  System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+                // Remove .dll from the end of the file path (macos stuff)
+                p.StartInfo.FileName = p.StartInfo.FileName.Substring(0, p.StartInfo.FileName.Length - 4);
+
+                p.StartInfo.Arguments = num.ToString();
+                p.StartInfo.UseShellExecute = true; // needed in macos
+                p.Start();
+
+                p.WaitForExit(); // Important
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("No or faulty arguments give.");
+        }
+
+        Console.WriteLine("Ending -- " + num.ToString());
     }
 }
-
-
-// command + . (saa lampun "quick fixes")
-
-
