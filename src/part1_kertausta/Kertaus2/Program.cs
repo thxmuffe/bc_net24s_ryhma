2d1a89de399c1ae2fee16cda77ae9bd91817c132
Paavo Penttila <thxmuffe@hotmail.com>
2025-01-20 12:57:16 +0200

    Add Serilog example

---
+++ src/part1_kertausta/Kertaus2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        logger.Information("Hello, world!");
+    }
+}
