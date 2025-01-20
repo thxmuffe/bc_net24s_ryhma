76a3b499b205bbd4e80e040cbea882809cf1d0ee
Paavo Penttila <thxmuffe@hotmail.com>
2025-01-20 10:36:23 +0200

    Do not require arguments (use default if none given)

--- src/part1_kertausta/Kertaus1/Program.cs
+++ src/part1_kertausta/Kertaus1/Program.cs
@@ -3,40 +3,36 @@ using System;
 
 internal class Program
 {
+    private const int MaximumNumberOfCalls = 3000;
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("start " + String.Join(", ", args));
-
         // Try to parse first argument as number
-        if (int.TryParse(args.FirstOrDefault(), out int num))
-        {
-            num++;
+        int.TryParse(args.FirstOrDefault() ?? "0", out int num);
 
-            if (num > 10) {
-                Console.WriteLine("Reached 10!");
-                Environment.Exit(0);
-            }
+        Console.WriteLine($"starting with arg: {num}");
 
-            using (var p = new Process()) {
-                // Get current program executable
-                p.StartInfo.FileName =  System.Reflection.Assembly.GetExecutingAssembly().Location;
-
-                // Remove .dll from the end of the file path (macos stuff)
-                p.StartInfo.FileName = p.StartInfo.FileName.Substring(0, p.StartInfo.FileName.Length - 4);
-
-                p.StartInfo.Arguments = num.ToString();
-                p.StartInfo.UseShellExecute = true; // needed in macos
-                p.Start();
-
-                p.WaitForExit(); // Important
-            }
-            
-        }
-        else
-        {
-            Console.WriteLine("No or faulty arguments give.");
+        num++;
+        
+        if (num > MaximumNumberOfCalls) {
+            Console.WriteLine($"Exceeded MaximumNumberOfCalls {MaximumNumberOfCalls}");
+            Environment.Exit(0);
         }
 
+        using (var p = new Process()) {
+            // Get current program executable
+            p.StartInfo.FileName =  System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            // Remove .dll from the end of the file path (macos stuff)
+            p.StartInfo.FileName = p.StartInfo.FileName.Substring(0, p.StartInfo.FileName.Length - 4);
+
+            p.StartInfo.Arguments = num.ToString();
+            p.StartInfo.UseShellExecute = true; // needed in macos
+            p.Start();
+
+            p.WaitForExit(); // Important
+        }
+        
         Console.WriteLine("Ending -- " + num.ToString());
     }
 }
